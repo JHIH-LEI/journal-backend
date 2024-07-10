707812c6f091edb6e7a153a4a31d4a4e88456c4a
Alicia <vicky88888@gmail.com>
2024-07-09 20:09:24 -0400

    feat: prisma schema and graphql server init

---
+++ src/db.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./prisma/prisma-client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+export const pool = new Pool({
+  connectionString,
+});
+
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+
+interface Context {
+  user: {
+    id: number;
+    userName: string;
+  };
+}
+
+async function startApolloServer() {
+  const server = new ApolloServer<Context>({ typeDefs, resolvers });
+
+  const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => {
+      // TODO: token: getToken(req.headers.authentication)
+      //   const token = req.headers.authorization || "";
+      return {
+        user: {
+          id: 1,
+          userName: "Alicia",
+        },
+      };
+    },
+  });
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
