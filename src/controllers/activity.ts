ccdbf87e6efa833eac8f1c88239c5183e357a587
Alicia <vicky88888@gmail.com>
2024-09-21 15:39:41 -0400

    feat: activity controller (create/update/remove)

---
+++ src/controllers/activity.ts
@@ -0,0 +1,41 @@
+import { prisma } from "../db";
+import { CreateActivityInput, UpdateActivityInput } from "../generated/graphql";
+
+export const activityController = {
+  createActivity: async (input: CreateActivityInput) => {
+    const activity = await prisma.activity.create({
+      data: {
+        icon: {
+          connect: {
+            id: input.iconId,
+          },
+        },
+        activityName: input.activityName,
+        group: {
+          connect: {
+            id: input.groupId,
+          },
+        },
+      },
+    });
+    return activity;
+  },
+  updateActivity: async (input: UpdateActivityInput) => {
+    const updateActivity = await prisma.activity.update({
+      where: {
+        id: input.id,
+      },
+      data: {
+        iconId: input.iconId,
+        activityName: input.activityName,
+      },
+    });
+    return updateActivity;
+  },
+  removeActivity: async (id: number) => {
+    const activity = await prisma.activity.delete({
+      where: { id },
+    });
+    return activity;
+  },
+};
