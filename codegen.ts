a9a0e9aacc1ece6064e52ad3c0870b6c29213aa7
Alicia <vicky88888@gmail.com>
2024-09-21 15:34:07 -0400

    refector: setting codegen type mapper

--- codegen.ts
+++ codegen.ts
@@ -1,17 +1,30 @@
-
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
   schema: "http://localhost:4000",
   generates: {
     "src/generated/graphql.ts": {
-      plugins: ["typescript", "typescript-resolvers"]
+      config: {
+        mappers: {
+          JournalEvent: "./src/prisma/prisma-client#Event",
+          Activity: "./src/prisma/prisma-client#Activity",
+          Icon: "./src/prisma/prisma-client#Icon",
+          User: "./src/prisma/prisma-client#User",
+          Group: "./src/prisma/prisma-client#Group",
+          Mood: "./src/prisma/prisma-client#Mood",
+          Journal: "./src/prisma/prisma-client#Journal",
+          JournalCategory: "./src/prisma/prisma-client#Category",
+          Track: "./src/prisma/prisma-client#Track",
+          TrackDisplayType: "./src/prisma/prisma-client#TrackDisplayType",
+        },
+      },
+      plugins: ["typescript", "typescript-resolvers"],
     },
     "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
-  }
+      plugins: ["introspection"],
+    },
+  },
 };
 
 export default config;
